208dd0a7daad1ab4fcc87276e85f2b0a72e615ce
Olawale <waleokemuyiwa@gmail.com>
2023-01-11 12:47:26 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const myName = process.env.NAME;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(myName + "rocks!");
+    await sleep(5000);
+  }
+}
+
+main();
